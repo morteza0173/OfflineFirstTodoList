ec04c7fd1489c6f5a5fd1a94c34663b36edf8a86
morteza0173 <morteza0173@outlook.com>
2025-09-10 12:51:42 +0330

    fix sw register

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,54 @@
-import createPWA from "next-pwa";
+/** @type {import('next').NextConfig} */
 
-const withPWA = createPWA({
-  dest: "public", 
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+import path from "path";
+
+const isDev = process.env.NODE_ENV !== "production";
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const withPWA = require("next-pwa")({
+  dest: "public",
+
+  disable: isDev,
+
+  buildExcludes: ["app-build-manifest.json"],
 });
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const generateAppDirEntry = (entry: () => Promise<any>) => {
+  const packagePath = require.resolve("next-pwa");
+
+  const packageDirectory = path.dirname(packagePath);
+
+  const registerJs = path.join(packageDirectory, "register.js");
+
+  return entry().then((entries) => {
+    // Register SW on App directory, solution: https://github.com/shadowwalker/next-pwa/pull/427
+
+    if (entries["main-app"] && !entries["main-app"].includes(registerJs)) {
+      if (Array.isArray(entries["main-app"])) {
+        entries["main-app"].unshift(registerJs);
+      } else if (typeof entries["main-app"] === "string") {
+        entries["main-app"] = [registerJs, entries["main-app"]];
+      }
+    }
+
+    return entries;
+  });
+};
 
 const nextConfig = {
   reactStrictMode: true,
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  webpack(config: { entry: { (): Promise<any>; (): Promise<any> } }) {
+    if (!isDev) {
+      const entry = generateAppDirEntry(config.entry);
+
+      config.entry = () => entry;
+    }
+
+    return config;
+  },
 };
 
-export default withPWA(nextConfig);
+module.exports = withPWA(nextConfig);
